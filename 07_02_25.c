ae17eb9140e5d40d2ac56c39df38f43f899e216f
JOTHIPALANISAMY <117978130+JOTHIPALANISAMY@users.noreply.github.com>
2025-02-07 16:17:06 +0530

    Update 07_02_25.c

--- 07_02_25.c
+++ 07_02_25.c
@@ -28,3 +28,69 @@ int main() {
     printf("%d^%d = %d\n", n, power, result);
     return 0;
 }
+
+
+//funtions without parameters
+#include <stdio.h>
+void add();//function declaration without parameters
+
+int main() {
+    add();//function call
+    return 0;
+}
+
+void add()//function definition
+{
+    int a=10,b=20; //function definition
+    int sum=a+b;
+    printf("%d",sum);
+}
+
+
+
+//add and sub without parameters
+#include <stdio.h>
+void add()//function declaration
+{
+    int a=10,b=20; //function definition
+    int sum=a+b;
+    printf("addition answer is %d\n",sum);
+}
+void sub()
+{
+   int a=10,b=20; //function definition
+    int sum=a-b;
+    printf("subtraction answer is %d",sum);
+}
+
+int main() {
+    add();//function call
+    sub();
+    return 0;
+}
+
+
+
+//functions with parameter
+#include <stdio.h>
+void add(int m,int n)//function definition with parameter
+{
+    int sum=m+n;
+    printf("addition answer is %d\n",sum);
+}
+void sub()
+{
+   int a=10,b=20; //function definition
+    int sum=a-b;
+    printf("subtraction answer is %d",sum);
+}
+
+int main() {
+    int a=10;
+    int b=20;
+    add(a,b);//function call
+    sub();
+    return 0;
+}
+
+
